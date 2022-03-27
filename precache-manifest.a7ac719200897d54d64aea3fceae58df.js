2b7947ba7b622673712b4d26f1a8d9d80a05c969
Luke Artnak <lartnak@gmail.com>
2022-03-26 23:52:17 -0400

    1a70c70

---
+++ precache-manifest.a7ac719200897d54d64aea3fceae58df.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "df564fb6406627513f6f",
+    "url": "/nhl-scores/static/js/main.df564fb6.chunk.js"
+  },
+  {
+    "revision": "a57ac099724e0761cdad",
+    "url": "/nhl-scores/static/js/1.a57ac099.chunk.js"
+  },
+  {
+    "revision": "64c0c4806a4b2bd60cd2",
+    "url": "/nhl-scores/static/js/runtime~main.64c0c480.js"
+  },
+  {
+    "revision": "c834c9237870dd564afb957962c6b04f",
+    "url": "/nhl-scores/index.html"
+  }
+];
