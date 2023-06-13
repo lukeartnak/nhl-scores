54599079a2346368582f178292074132c0acb232
Luke Artnak <lartnak@gmail.com>
2023-06-12 20:54:45 -0400

    cf8450b

---
+++ precache-manifest.92da3f130a13ff34f728bc4d35883502.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "585999aaec5aa1d90e06",
+    "url": "/nhl-scores/static/js/main.585999aa.chunk.js"
+  },
+  {
+    "revision": "8dc002eb4a9776415edb",
+    "url": "/nhl-scores/static/js/1.8dc002eb.chunk.js"
+  },
+  {
+    "revision": "64c0c4806a4b2bd60cd2",
+    "url": "/nhl-scores/static/js/runtime~main.64c0c480.js"
+  },
+  {
+    "revision": "ba88add1a2985de80976b712b75c41cc",
+    "url": "/nhl-scores/index.html"
+  }
+];
